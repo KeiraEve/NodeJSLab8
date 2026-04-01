f6c1aae04e60bfe4b9b48f0aec31f35bf23cdb48
Keira C. <ke07chap@siena.edu>
2026-04-01 16:18:47 +0000

    Began B.7: Message Board.

---
+++ fetch-lab/server.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+// Serve static files from the 'public' folder
+app.use(express.static('public'));
+
+// Parse JSON request bodies (needed for POST)
+app.use(express.json());
+
+// ---- Your endpoints go below this line ----
+
+
+
+// ---- Your endpoints go above this line ----
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/hello', (req, res) => {
+    res.type('text').send('Hello from the server!');
+});
+
+app.get("/api/time", (req, res) => {
+    let response = {
+        "currentTime": new Date().toISOString(),
+        "message": "Current server time"
+    };
+
+    res.json(response);
+});
+
+app.get("/api/greet/:name", (req, res) => {
+    let response = {
+        "greeting": `Hello, ${req.params.name}! Welcome to the API.`
+    };
+
+    res.json(response);
+});
+
+app.get("/api/math", (req, res) => {
+    let a = req.query.a;
+    let b = req.query.b;
+    let operation = req.query.operation;
+    let result = 0;
+
+    if (operation == "add") {
+        result = a + b;
+    } else if (operation == "subtract") {
+        result = a - b;
+    } else if (operation == "divide") {
+        if (b == 0) {
+            res.status(400).json({ "error": "Cannot divide by zero" });
+        } else {
+            result = a / b;
+        }
+    } else if (operation == "multiply") {
+        result = a * b;
+    } else {
+        res.status(400).json({ "error": "Invalid or missing operation. Use: add, subtract, multiply, divide" });
+    }
+
+    let response = {
+        "a": a,
+        "b": b,
+        "operation": operation,
+        "result": result
+    }
+    res.json(response);
+});
+
+app.get("/api/slow", (req, res) => {
+    setTimeout(() => {
+        res.json({
+            message: "Sorry for the wait!",
+            delayMS: 3000
+        });
+    }, 3000);
+});
+
+app.get("/api/unreliable", (req, res) => {
+    const rand = Math.random();
+    if (rand < 0.5) {
+        res.status(500).json({
+            error: "Server had a bad day. Try again!"
+        });
+    } else {
+        res.json({
+            message: "Lucky! It worked this time.",
+            luckyNumber: Math.floor(Math.random() * 100)
+        });
+    }
+});
+
+app.get("/api/messages", (req, res) => {
+
+});
+
+app.post("/api/messages", (req, res) => {
+
+});
